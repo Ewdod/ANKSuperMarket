4f54a78832a52a94591eb6b93d94dc69afb19f05
haybek <haybekbatuhan@gmail.com>
2023-08-08 15:47:28 +0300

    Add project files.

---
+++ ANKSuperMarket/Data/MarketDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ANKSuperMarket.Data
+{
+    public class MarketDbContext : DbContext
+    {
+
+        public MarketDbContext(DbContextOptions<MarketDbContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Urun> Urunler => Set<Urun>();
+    }
+    
+}
---
+++ ANKSuperMarket/Data/Urun.cs
@@ -0,0 +1,9 @@
+﻿namespace ANKSuperMarket.Data
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Fiyat { get; set; }
+    }
+}
